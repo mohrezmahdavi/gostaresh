09be6c7b7fda34f6261b3e07865382379b7c4443
mahdavi <m.mahdavi@raahin.com>
2022-06-30 12:57:06 +0430

    industrialExpenditureResearches list item completed

--- resources/views/admin/gostaresh/industrial-expenditure-research/list/list.blade.php
+++ resources/views/admin/gostaresh/industrial-expenditure-research/list/list.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.dashboard')
 
 @section('title-tag')
+روند تغییرات درصد هزینه‌کرد بخش صنعت در تحقیق‌ و توسعه
 @endsection
 
 @section('breadcrumb-title')
+روند تغییرات درصد هزینه‌کرد بخش صنعت در تحقیق‌ و توسعه
 @endsection
 
 @section('page-title')
+روند تغییرات درصد هزینه‌کرد بخش صنعت در تحقیق‌ و توسعه
 @endsection
 
 @section('styles-head')
@@ -15,10 +18,52 @@
 @section('content')
     @include('admin.partials.row-notifiy-col')
 
-
     <div class="row">
         <div class="col-md-12">
-
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table mb-0">
+                            <thead class="thead-light">
+        
+                                <tr>
+                                    <th>#</th>
+                                    <th>شهرستان </th>
+                                    <th>مقدار </th>
+                                    <th>سال</th>
+                                    <th>اقدام</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($industrialExpenditureResearches as $key => $industrialExpenditureResearch)
+                                    <tr>
+                                        <th scope="row">{{ $industrialExpenditureResearches?->firstItem() + $key }}</th>
+        
+                                        <td>{{ $industrialExpenditureResearch?->province?->name . ' - ' . $industrialExpenditureResearch->county?->name }}
+                                        </td>
+                                        <td>{{ $industrialExpenditureResearch?->amount }}</td>
+                                        <td>{{ $industrialExpenditureResearch?->year }}</td>
+                                        <td>
+        
+                                            <a href="{{ route('industrial.expenditure.research.edit', $industrialExpenditureResearch) }}"
+                                                title="{{ __('validation.buttons.edit') }}" class="btn btn-warning btn-sm"><i
+                                                    class="fa fa-edit"></i></a>
+        
+                                            <a href="{{ route('industrial.expenditure.research.destroy', $industrialExpenditureResearch) }}" title="{{ __('validation.buttons.delete') }}"
+                                                class="btn btn-danger btn-sm"><i class="fa fa-minus"></i></a>
+                                        </td>
+        
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+        
+                    </div> <!-- end table-responsive-->
+                    <div class="mt-3">
+                        {{ $industrialExpenditureResearches->withQueryString()->links('pagination::bootstrap-4') }}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
