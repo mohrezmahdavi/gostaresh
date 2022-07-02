7679d4b39c6f35389a98bf065ab1c8b62cfc8d75
mahdavi <m.mahdavi@raahin.com>
2022-07-02 11:06:11 +0430

    InternationalStudentGrowthRate crud completed

--- resources/views/admin/gostaresh/international-student-growth-rate/list/list.blade.php
+++ resources/views/admin/gostaresh/international-student-growth-rate/list/list.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.dashboard')
 
 @section('title-tag')
+نرخ رشد دانشجویان غیرایرانی و بین الملل واحدهای دانشگاهی
 @endsection
 
 @section('breadcrumb-title')
+نرخ رشد دانشجویان غیرایرانی و بین الملل واحدهای دانشگاهی
 @endsection
 
 @section('page-title')
+نرخ رشد دانشجویان غیرایرانی و بین الملل واحدهای دانشگاهی
 @endsection
 
 @section('styles-head')
@@ -15,10 +18,64 @@
 @section('content')
     @include('admin.partials.row-notifiy-col')
 
-
     <div class="row">
         <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table mb-0">
+                            <thead class="thead-light">
+        
+                                <tr>
+                                    <th>#</th>
+                                    <th>شهرستان</th>
+                                    <th>واحد دانشگاهی </th>
+                                    <th>رشته تحصیلی</th>
+                                    <th>جنسیت</th>
+                                    <th>کاردانی</th>
+                                    <th>کارشناسی</th>
+                                    <th>کارشناسی ارشد</th>
+                                    <th>دکتری</th>
+                                    <th>اقدام</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($internationalStudentGrowthRates as $key => $internationalStudentGrowthRate)
+                                    <tr>
+                                        <th scope="row">{{ $internationalStudentGrowthRates?->firstItem() + $key }}</th>
+        
+                                        <td>{{ $internationalStudentGrowthRate?->province?->name . ' - ' . $internationalStudentGrowthRate->county?->name }}
+                                        </td>
+                                        <td>{{ $internationalStudentGrowthRate?->unit }}</td>
+                                        <td>{{ $internationalStudentGrowthRate?->gender_title }}</td>
+                                        <td>{{ $internationalStudentGrowthRate?->department_of_education_title }}</td>
+                                        <td>{{ $internationalStudentGrowthRate?->kardani_count }}</td>
+                                        <td>{{ $internationalStudentGrowthRate?->karshenasi_count }}</td>
+                                        <td>{{ $internationalStudentGrowthRate?->karshenasi_arshad_count }}</td>
+                                        <td>{{ $internationalStudentGrowthRate?->docktora_count }}</td>
 
+                                        <td>{{ $internationalStudentGrowthRate?->year }}</td>
+                                        <td>
+        
+                                            <a href="{{ route('international.student.growth.rate.edit', $internationalStudentGrowthRate) }}"
+                                                title="{{ __('validation.buttons.edit') }}" class="btn btn-warning btn-sm"><i
+                                                    class="fa fa-edit"></i></a>
+        
+                                            <a href="{{ route('international.student.growth.rate.destroy', $internationalStudentGrowthRate) }}" title="{{ __('validation.buttons.delete') }}"
+                                                class="btn btn-danger btn-sm"><i class="fa fa-minus"></i></a>
+                                        </td>
+        
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+        
+                    </div> <!-- end table-responsive-->
+                    <div class="mt-3">
+                        {{ $internationalStudentGrowthRates->withQueryString()->links('pagination::bootstrap-4') }}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
