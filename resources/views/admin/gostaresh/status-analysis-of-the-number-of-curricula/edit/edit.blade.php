50f2320b37e242111607469ffaf5a6c4726a3bd0
mahdavi <m.mahdavi@raahin.com>
2022-07-03 15:20:19 +0430

    component year and month add to forms completed

--- resources/views/admin/gostaresh/status-analysis-of-the-number-of-curricula/edit/edit.blade.php
+++ resources/views/admin/gostaresh/status-analysis-of-the-number-of-curricula/edit/edit.blade.php
@@ -23,7 +23,6 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body" id="app">
-                    @include('admin.partials.row-notifiy-col')
                     <form class="form-horizontal" method="POST" action="{{ route('status.analysis.of.the.number.of.curricula.update', $stsAnalysisOfTheNumOfCurricula) }}" role="form">
                         @csrf
                         @method('PUT')
@@ -118,41 +117,11 @@
                             </div>
                         </div>
 
+                        <x-select-year :default="$stsAnalysisOfTheNumOfCurricula->year" :required="false" name="year"></x-select-year>
 
-                        <div class="form-group row mt-2">
-                            <label class="col-sm-2 col-form-label" for="year">
-                                <span> سال </span>&nbsp
-                                <span class="text-danger" style="font-size: 11px !important"> (اجباری) </span>
-                            </label>
-                            <div class="col-sm-10">
-                                <select name="year" id="year" class="form-select">
-                                    @for ($i = 1250; $i <= 1405; $i++)
-                                        <option {{ $i == $stsAnalysisOfTheNumOfCurricula->year ? 'selected' : '' }} value="{{ $i }}">
-                                            {{ $i }}</option>
-                                    @endfor
+                        <x-select-month :default="$stsAnalysisOfTheNumOfCurricula->month" :required="false" name="month"></x-select-month>
 
-                                </select>
-
-                            </div>
-                        </div>
-
-                        <div class="form-group row mt-2">
-                            <label class="col-sm-2 col-form-label" for="month">
-                                <span> ماه </span>&nbsp
-                                <span class="text-danger" style="font-size: 11px !important"> (اجباری) </span>
-                            </label>
-                            <div class="col-sm-10">
-                                <select name="month" id="month" class="form-select">
-                                    @for ($i = 1; $i <= 12; $i++)
-                                        <option {{ $i == $stsAnalysisOfTheNumOfCurricula->month ? 'selected' : '' }} value="{{ $i }}">
-                                            {{ $i }}</option>
-                                    @endfor
-
-                                </select>
-
-                            </div>
-                        </div>
-
+                        
                         <button type="submit" class="btn btn-primary  mt-3">ویرایش</button>
                     </form>
                 </div>
--- resources/views/admin/gostaresh/technological-product/edit/edit.blade.php
+++ resources/views/admin/gostaresh/technological-product/edit/edit.blade.php
@@ -24,7 +24,6 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body" id="app">
-                    @include('admin.partials.row-notifiy-col')
                     <form class="form-horizontal" method="POST"
                         action="{{ route('technological-product.update', $technologicalProduct) }}"
                         role="form">
@@ -168,42 +167,12 @@
                                        placeholder=" تعداد دانشجوی فناور فعال را وارد کنید...">
                             </div>
                         </div>
+
+                        <x-select-year :default="$technologicalProduct->year" :required="false" name="year"></x-select-year>
+
+                        <x-select-month :default="$technologicalProduct->month" :required="false" name="month"></x-select-month>
                         
-                        <div class="form-group row mt-2">
-                            <label class="col-sm-2 col-form-label" for="year">
-                                <span> سال </span>&nbsp
-                                <span class="text-danger" style="font-size: 11px !important"> (اجباری) </span>
-                            </label>
-                            <div class="col-sm-10">
-                                <select name="year" id="year" class="form-select">
-                                    @for ($i = 1250; $i <= 1405; $i++)
-                                        <option {{ $i == $technologicalProduct->year ? 'selected' : '' }}
-                                            value="{{ $i }}">
-                                            {{ $i }}</option>
-                                    @endfor
-
-                                </select>
-
-                            </div>
-                        </div>
-
-                        <div class="form-group row mt-2">
-                            <label class="col-sm-2 col-form-label" for="month">
-                                <span> ماه </span>&nbsp
-                                <span class="text-danger" style="font-size: 11px !important"> (اجباری) </span>
-                            </label>
-                            <div class="col-sm-10">
-                                <select name="month" id="month" class="form-select">
-                                    @for ($i = 1; $i <= 12; $i++)
-                                        <option {{ $i == $technologicalProduct->month ? 'selected' : '' }}
-                                            value="{{ $i }}">
-                                            {{ $i }}</option>
-                                    @endfor
-
-                                </select>
-
-                            </div>
-                        </div>
+                        
 
                         <button type="submit" class="btn btn-primary  mt-3">ویرایش</button>
                     </form>
