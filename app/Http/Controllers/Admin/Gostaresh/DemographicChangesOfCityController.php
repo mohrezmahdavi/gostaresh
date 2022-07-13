0c1defa4ec38f45f3dbc9b4494ba409ca11e3827
mahdavi <m.mahdavi@raahin.com>
2022-07-13 16:30:29 +0430

    whereRequestsQuery query and dynamic chart in DemographicChangesOfCity completed

--- app/Http/Controllers/Admin/Gostaresh/DemographicChangesOfCityController.php
+++ app/Http/Controllers/Admin/Gostaresh/DemographicChangesOfCityController.php
@@ -23,9 +23,8 @@ class DemographicChangesOfCityController extends Controller
      */
     public function index()
     {
-        $query = $this->getDemographicChangesOfCitiesQuery();
+        $query = DemographicChangesOfCity::whereRequestsQuery();
         $yearSelectedList = $this->yearSelectedList(clone $query);
-        $query = filterByOwnProvince($query);
         $demographicChangesOfCities = $query->orderBy('id', 'desc')->paginate(20);
         return view('admin.gostaresh.demographic-changes-of-city.list.list', compact('demographicChangesOfCities', 'yearSelectedList'));
     }
@@ -35,50 +34,10 @@ class DemographicChangesOfCityController extends Controller
         return $query->select('year')->pluck('year');
     }
 
-    private function getDemographicChangesOfCitiesQuery()
-    {
-        $query = DemographicChangesOfCity::query();
-
-        if (request()->province_id)
-            $query->where('province_id', request()->province_id);
-
-        if (request()->county_id)
-            $query->where('county_id', request()->county_id);
-
-        if (request()->city_id)
-            $query->where('city_id', request()->city_id);
-
-        if (request()->rural_district_id)
-            $query->where('rural_district_id', request()->rural_district_id);
-
-        if (request()->input('start_date')) {
-            $startDateJ = Verta::instance(request()->input('start_date'));
-            $startMonth = (int)$startDateJ->format('n');
-            $startYear = (int)$startDateJ->format('Y');
-            $query->where('year', '>', $startYear)->orWhere(function ($query) use ($startYear, $startMonth) {
-                $query->where('year', $startYear)->where('month', '>', $startMonth);
-            });
-        }
-
-        if (request()->input('end_date')) {
-            $endDateJ = Verta::instance(request()->input('end_date'));
-            $endMonth = (int)$endDateJ->format('n');
-            $endYear = (int)$endDateJ->format('Y');
-            $query->where('year', '<=', $endYear)->orWhere(function ($query) use ($endYear, $endMonth) {
-                $query->where('year', $endYear)->where('month', '<=', $endMonth);
-            });
-        }
-
-        if (request()->year) {
-            $query->where('year', request()->year);
-        }
-
-        return $query;
-    }
 
     public function listExcelExport()
     {
-        $query = $this->getDemographicChangesOfCitiesQuery();
+        $query = DemographicChangesOfCity::whereRequestsQuery();
         $demographicChangesOfCities = $query->orderBy('id', 'desc')->get();
         return Excel::download(new ListExport($demographicChangesOfCities), 'invoices.xlsx');
     }
