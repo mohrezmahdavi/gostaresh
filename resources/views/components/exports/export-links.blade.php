2650c86b64c886ec51dc4f6204075678a61d301f
hadikhah <ma.hadikhah@raahin.com>
2022-07-24 13:52:15 +0430

    fixes missing columns of the export list when selecting a column

--- resources/views/components/exports/export-links.blade.php
+++ resources/views/components/exports/export-links.blade.php
@@ -1,12 +1,12 @@
 @if ($excelLink ?? null != null)
-    <a href="{{ $excelLink }}" class="btn btn-success" style="background-color: rgb(1, 143, 157)">
+    <a href="{!! $excelLink !!}" class="btn btn-success" style="background-color: rgb(1, 143, 157)">
         خروجی اکسل
         <i class="fa fa-file-excel"></i>
     </a>
 @endif
 
 @if ($pdfLink ?? null != null)
-    <a href="{{ $pdfLink }}" class="btn btn-success" style="background-color: rgb(0, 188, 217)">
+    <a href="{!! $pdfLink !!}" class="btn btn-success" style="background-color: rgb(0, 188, 217)">
         خروجی
         PDF
         <i class="fa fa-file-pdf"></i>
@@ -14,7 +14,7 @@
 @endif
 
 @if ($printLink ?? null != null)
-    <a href="{{ $printLink }}" target="_blank" class="btn btn-success" style="background-color: rgb(68, 168, 228)">
+    <a href="{!! $printLink !!}" target="_blank" class="btn btn-success" style="background-color: rgb(68, 168, 228)">
         پرینت
         <i class="fa fa-print"></i>
     </a>
