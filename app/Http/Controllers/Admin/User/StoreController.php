1b5a930a210b9251fc125e2327873524040feda9
mahdavi <m.mahdavi@raahin.com>
2022-07-24 09:53:29 +0430

    fix bug GOS-159

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -14,7 +14,16 @@ class StoreController extends Controller
         if(!auth()->user()->hasPermissionTo('create-any-user'))
             abort(403);
 
-        $user = User::create(Arr::except($request->validated(),'roles'));
+        $user = User::create([
+            'first_name' => (string) $request->first_name,
+            'last_name' => (string) $request->last_name,
+            'status' => (int) $request->status,
+            'phone_number' => (string) $request->phone_number,
+            'province_id' =>  ($request->province_id != null) ? (int)$request->province_id : null,
+            'county_id' => ($request->county_id != null) ? (int)$request->county_id : null,
+            'city_id' => ($request->city_id != null) ? (int)$request->city_id : null,
+            'rural_district_id' => ($request->rural_district_id != null) ? (int)$request->rural_district_id : null,
+        ]);
         $user->syncRoles($request->roles);
 
         return redirect()->route('admin.users.list')->with('success','کاربر مورد نظر با موفقیت ایجاد شد.');
