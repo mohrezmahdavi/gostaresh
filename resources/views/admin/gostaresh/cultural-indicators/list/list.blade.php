84fc90ae42728eadfc0189ee0fb9e5507e2eff82
mahdavi <m.mahdavi@raahin.com>
2022-07-27 16:20:31 +0430

    GOS-260 fix

--- resources/views/admin/gostaresh/cultural-indicators/list/list.blade.php
+++ resources/views/admin/gostaresh/cultural-indicators/list/list.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.dashboard')
 
 @section('title-tag')
-تحلیل وضعیت شاخص ها و برنامه های فرهنگی
+    تحلیل وضعیت شاخص ها و برنامه های فرهنگی
 @endsection
 
 @section('breadcrumb-title')
-تحلیل وضعیت شاخص ها و برنامه های فرهنگی
+    تحلیل وضعیت شاخص ها و برنامه های فرهنگی
 @endsection
 
 @section('page-title')
-تحلیل وضعیت شاخص ها و برنامه های فرهنگی
+    تحلیل وضعیت شاخص ها و برنامه های فرهنگی
 
     <span>
         <a href="{{ route('admin.index') }}" class="btn btn-info btn-sm">بازگشت به منو</a>
@@ -26,8 +26,12 @@
     @include('admin.partials.row-notifiy-col')
 
 
-    <x-gostaresh.filter-table-list.filter-table-list-component :filterColumnsCheckBoxes="$filterColumnsCheckBoxes"
-                                                               :yearSelectedList="$yearSelectedList"/>
+    <x-gostaresh.filter-table-list.filter-table-list-component :filterColumnsCheckBoxes="$filterColumnsCheckBoxes" :yearSelectedList="$yearSelectedList" :fieldsProvinceSelect="[
+        'province' => true,
+        'zone' => false,
+        'county' => true,
+        'city' => false,
+    ]" />
 
     <div class="row">
         <div class="col-md-12">
@@ -37,56 +41,55 @@
                         <table class="table mb-0">
                             <thead class="thead-light">
 
-                            <tr>
-                                <th>#</th>
-                                <th>شهرستان</th>
-                                @foreach($filterColumnsCheckBoxes as $key => $value)
-                                    @if( filterCol($key))
-                                        <th>{{$value}}</th>
-                                    @endif
-                                @endforeach
-                                <th>سال</th>
-                                <th>اقدام</th>
-                            </tr>
-                            </thead>
-                            <tbody style="text-align: right; direction: ltr">
-                            @foreach ($culturalIndicators as $key => $culturalIndicator)
                                 <tr>
-                                    <th scope="row">{{ $culturalIndicators?->firstItem() + $key }}</th>
-                                    <td>{{ $culturalIndicator?->province?->name . ' - ' . $culturalIndicator->county?->name }}
-                                    @foreach( $filterColumnsCheckBoxes as $key => $value)
-                                        @if( filterCol($key))
-                                            @if(in_array($key , \App\Models\Index\CulturalIndicatorsStatusAnalysis::$numeric_fields))
-                                                <td>{{ number_format($culturalIndicator?->{$key}) }}</td>
-                                            @else
-                                                <td>{{ $culturalIndicator?->{$key} }}</td>
-                                            @endif
+                                    <th>#</th>
+                                    <th>شهرستان</th>
+                                    @foreach ($filterColumnsCheckBoxes as $key => $value)
+                                        @if (filterCol($key))
+                                            <th>{{ $value }}</th>
                                         @endif
                                     @endforeach
+                                    <th>سال</th>
+                                    <th>اقدام</th>
+                                </tr>
+                            </thead>
+                            <tbody style="text-align: right; direction: ltr">
+                                @foreach ($culturalIndicators as $key => $culturalIndicator)
+                                    <tr>
+                                        <th scope="row">{{ $culturalIndicators?->firstItem() + $key }}</th>
+                                        <td>{{ $culturalIndicator?->province?->name . ' - ' . $culturalIndicator->county?->name }}
+                                            @foreach ($filterColumnsCheckBoxes as $key => $value)
+                                                @if (filterCol($key))
+                                                    @if (in_array($key, \App\Models\Index\CulturalIndicatorsStatusAnalysis::$numeric_fields))
+                                        <td>{{ number_format($culturalIndicator?->{$key}) }}</td>
+                                    @else
+                                        <td>{{ $culturalIndicator?->{$key} }}</td>
+                                @endif
+                                @endif
+                                @endforeach
 
-                                    <td>{{ $culturalIndicator?->year }}</td>
-                                    <td>
+                                <td>{{ $culturalIndicator?->year }}</td>
+                                <td>
 
-                                        <a href="{{ route('cultural-indicators.edit', $culturalIndicator) }}"
-                                           title="{{ __('validation.buttons.edit') }}" class="btn btn-warning btn-sm"><i
-                                                class="fa fa-edit"></i></a>
+                                    <a href="{{ route('cultural-indicators.edit', $culturalIndicator) }}"
+                                        title="{{ __('validation.buttons.edit') }}" class="btn btn-warning btn-sm"><i
+                                            class="fa fa-edit"></i></a>
 
-                                        {{--                                        <a href="{{ route('research-output-status-analyses.destroy', $culturalIndicator) }}" title="{{ __('validation.buttons.delete') }}"--}}
-                                        {{--                                           class="btn btn-danger btn-sm"><i class="fa fa-minus"></i></a>--}}
+                                    {{-- <a href="{{ route('research-output-status-analyses.destroy', $culturalIndicator) }}" title="{{ __('validation.buttons.delete') }}" --}}
+                                    {{-- class="btn btn-danger btn-sm"><i class="fa fa-minus"></i></a> --}}
 
-                                    </td>
+                                </td>
 
                                 </tr>
-                            @endforeach
+                                @endforeach
                             </tbody>
                         </table>
 
                         <div class="text-end mt-3">
-                            <x-exports.export-links 
+                            <x-exports.export-links
                                 excelLink="{{ route('cultural-indicators.list.excel', request()->query->all()) }}"
                                 pdfLink="{{ route('cultural-indicators.list.pdf', request()->query->all()) }}"
-                                printLink="{{ route('cultural-indicators.list.print', request()->query->all()) }}"
-                            />
+                                printLink="{{ route('cultural-indicators.list.print', request()->query->all()) }}" />
                         </div>
                     </div>
                     <!-- end table-responsive-->
