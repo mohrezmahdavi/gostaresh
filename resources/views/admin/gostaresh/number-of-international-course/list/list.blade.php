bc7ed55185b892a7ee0dd268629159b136e70c64
mahdavi <m.mahdavi@raahin.com>
2022-07-27 16:49:06 +0430

    GOS-249 fix

--- resources/views/admin/gostaresh/number-of-international-course/list/list.blade.php
+++ resources/views/admin/gostaresh/number-of-international-course/list/list.blade.php
@@ -1,25 +1,24 @@
 @extends('layouts.dashboard')
 
 @section('title-tag')
-تعداد دانشجویان غیرایرانی و بین الملل واحدهای دانشگاهی استان
+    تعداد دانشجویان غیرایرانی و بین الملل واحدهای دانشگاهی استان
 @endsection
 
 @section('breadcrumb-title')
-تعداد دانشجویان غیرایرانی و بین الملل واحدهای دانشگاهی استان
+    تعداد دانشجویان غیرایرانی و بین الملل واحدهای دانشگاهی استان
 @endsection
 
 @section('page-title')
-تعداد دانشجویان غیرایرانی و بین الملل واحدهای دانشگاهی استان
-<span>
-    <a href="{{ route('admin.index') }}" class="btn btn-info btn-sm">بازگشت به منو</a>
-</span>
-<span>
-    <a href="{{ route('number.of.international.course.create') }}" class="btn btn-success btn-sm">افزودن رکورد جدید</a>
-</span>
+    تعداد دانشجویان غیرایرانی و بین الملل واحدهای دانشگاهی استان
+    <span>
+        <a href="{{ route('admin.index') }}" class="btn btn-info btn-sm">بازگشت به منو</a>
+    </span>
+    <span>
+        <a href="{{ route('number.of.international.course.create') }}" class="btn btn-success btn-sm">افزودن رکورد جدید</a>
+    </span>
 @endsection
 
 @section('styles-head')
-
 @endsection
 
 @section('content')
@@ -27,8 +26,12 @@
 
 
 
-    <x-gostaresh.filter-table-list.filter-table-list-component :filterColumnsCheckBoxes="$filterColumnsCheckBoxes"
-                                                               :yearSelectedList="$yearSelectedList"/>
+    <x-gostaresh.filter-table-list.filter-table-list-component :filterColumnsCheckBoxes="$filterColumnsCheckBoxes" :yearSelectedList="$yearSelectedList" :fieldsProvinceSelect="[
+        'province' => true,
+        'zone' => false,
+        'county' => true,
+        'city' => false,
+    ]" />
 
     <div class="row">
         <div class="col-md-12">
@@ -52,14 +55,15 @@
 
                                         @include('admin.gostaresh.number-of-international-course.list.partials.tbody')
 
-                                        
+
                                         <td>
 
                                             <a href="{{ route('number.of.international.course.edit', $numberOfInternationalCourse) }}"
-                                                title="{{ __('validation.buttons.edit') }}" class="btn btn-warning btn-sm"><i
-                                                    class="fa fa-edit"></i></a>
+                                                title="{{ __('validation.buttons.edit') }}"
+                                                class="btn btn-warning btn-sm"><i class="fa fa-edit"></i></a>
 
-                                            <a href="{{ route('number.of.international.course.destroy', $numberOfInternationalCourse) }}" title="{{ __('validation.buttons.delete') }}"
+                                            <a href="{{ route('number.of.international.course.destroy', $numberOfInternationalCourse) }}"
+                                                title="{{ __('validation.buttons.delete') }}"
                                                 class="btn btn-danger btn-sm"><i class="fa fa-minus"></i></a>
                                         </td>
 
@@ -68,11 +72,10 @@
                             </tbody>
                         </table>
                         <div class="text-end mt-3">
-                            <x-exports.export-links 
+                            <x-exports.export-links
                                 excelLink="{{ route('number.of.international.course.list.excel', request()->query->all()) }}"
                                 pdfLink="{{ route('number.of.international.course.list.pdf', request()->query->all()) }}"
-                                printLink="{{ route('number.of.international.course.list.print', request()->query->all()) }}"
-                            />
+                                printLink="{{ route('number.of.international.course.list.print', request()->query->all()) }}" />
                         </div>
                     </div> <!-- end table-responsive-->
                     <div class="mt-3">
@@ -85,7 +88,5 @@
 @endsection
 
 @section('body-scripts')
-
     <script src="{{ mix('/js/app.js') }}"></script>
-
 @endsection
