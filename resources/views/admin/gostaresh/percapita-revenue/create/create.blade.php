891f83313c937ba0c6c8f850cd96ce6e46b66faa
mahdavi <m.mahdavi@raahin.com>
2022-07-27 15:55:03 +0430

    GOS-269 fix

--- resources/views/admin/gostaresh/percapita-revenue/create/create.blade.php
+++ resources/views/admin/gostaresh/percapita-revenue/create/create.blade.php
@@ -1,20 +1,20 @@
-{{--Table 51 View--}}
+{{-- Table 51 View --}}
 @extends('layouts.dashboard')
 
 @section('title-tag')
-ایجاد تحلیل وضعیت درآمد سرانه مقطع-رشتھ-گرایش-محل x شھرستان ھای استان
+    ایجاد تحلیل وضعیت درآمد سرانه (مقطع-رشته-گرایش-محل) شھرستان ھای استان
 @endsection
 
 @section('breadcrumb-title')
-ایجاد تحلیل وضعیت درآمد سرانه مقطع-رشتھ-گرایش-محل x شھرستان ھای استان
+    ایجاد تحلیل وضعیت درآمد سرانه (مقطع-رشته-گرایش-محل) شھرستان ھای استان
 @endsection
 
 @section('page-title')
-ایجاد تحلیل وضعیت درآمد سرانه مقطع-رشتھ-گرایش-محل x شھرستان ھای استان
+    ایجاد تحلیل وضعیت درآمد سرانه (مقطع-رشته-گرایش-محل) شھرستان ھای استان
 
-<span>
-    <a href="{{ route('admin.index') }}" class="btn btn-info btn-sm">بازگشت به منو</a>
-</span>
+    <span>
+        <a href="{{ route('admin.index') }}" class="btn btn-info btn-sm">بازگشت به منو</a>
+    </span>
 @endsection
 
 @section('styles-head')
@@ -28,15 +28,21 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body" id="app">
-                    <form class="form-horizontal" method="POST" action="{{ route('percapita-revenue.store') }}" role="form">
+                    <form class="form-horizontal" method="POST" action="{{ route('percapita-revenue.store') }}"
+                        role="form">
                         @csrf
 
-                        <select-province-component
-                            province_default="{{ auth()->user()->province_id ?? '' }}"
-                            zone_default="{{ auth()->user()->county->zone ?? ''}}"
+                        <select-province-component province_default="{{ auth()->user()->province_id ?? '' }}"
+                            zone_default="{{ auth()->user()->county->zone ?? '' }}"
                             county_default="{{ auth()->user()->county_id ?? '' }}"
                             city_default="{{ auth()->user()->city_id ?? '' }}"
-                            rural_district_default="{{ auth()->user()->rural_district_id ?? '' }}">
+                            rural_district_default="{{ auth()->user()->rural_district_id ?? '' }}"
+                            :fields="{{ json_encode([
+                                'province' => true,
+                                'zone' => false,
+                                'county' => true,
+                                'city' => false,
+                            ]) }}">
                         </select-province-component>
 
                         <select-grade-component></select-grade-component>
@@ -47,21 +53,21 @@
                                 <span class="text-danger" style="font-size: 11px !important"> (اجباری) </span>
                             </label>
                             <div class="col-sm-10">
-                                <input type="text" id="unit" name="unit"
-                                       value="{{ old('unit') }}" class="form-control"
-                                       placeholder=" واحد را وارد کنید...">
+                                <input type="text" id="unit" name="unit" value="{{ old('unit') }}"
+                                    class="form-control" placeholder=" واحد را وارد کنید...">
                             </div>
                         </div>
 
                         <div class="form-group row mt-2">
                             <label class="col-sm-2 col-form-label" for="university_type">
-                                <span>دانشگاه  </span>&nbsp
+                                <span>دانشگاه </span>&nbsp
                                 <span class="text-danger" style="font-size: 11px !important"> (اجباری) </span>
                             </label>
                             <div class="col-sm-10">
-                                <select name="university_type" id="university_type" class="form-select" >
+                                <select name="university_type" id="university_type" class="form-select">
                                     @foreach (config('gostaresh.university_type') as $key => $value)
-                                        <option {{ ($key == old('university_type') ? 'selected' : '') }} value="{{ $key }}">{{ $value }}</option>
+                                        <option {{ $key == old('university_type') ? 'selected' : '' }}
+                                            value="{{ $key }}">{{ $value }}</option>
                                     @endforeach
                                 </select>
 
@@ -70,13 +76,14 @@
 
                         <div class="form-group row mt-2">
                             <label class="col-sm-2 col-form-label" for="grade">
-                                <span>مقطع تحصیلی  </span>&nbsp
+                                <span>مقطع تحصیلی </span>&nbsp
                                 <span class="text-danger" style="font-size: 11px !important"> (اجباری) </span>
                             </label>
                             <div class="col-sm-10">
-                                <select name="grade" id="grade" class="form-select" >
+                                <select name="grade" id="grade" class="form-select">
                                     @foreach (config('gostaresh.grade') as $key => $value)
-                                        <option {{ ($key == old('grade') ? 'selected' : '') }} value="{{ $key }}">{{ $value }}</option>
+                                        <option {{ $key == old('grade') ? 'selected' : '' }}
+                                            value="{{ $key }}">{{ $value }}</option>
                                     @endforeach
                                 </select>
 
@@ -89,15 +96,16 @@
                                 <span class="text-danger" style="font-size: 11px !important"> (اجباری) </span>
                             </label>
                             <div class="col-sm-10">
-                                <input type="text" id="percapita_revenue_status_analyses" name="percapita_revenue_status_analyses"
-                                       value="{{ old('percapita_revenue_status_analyses') }}" class="form-control"
-                                       placeholder=" تحلیل وضعیت درآمد سرانه را وارد کنید...">
+                                <input type="text" id="percapita_revenue_status_analyses"
+                                    name="percapita_revenue_status_analyses"
+                                    value="{{ old('percapita_revenue_status_analyses') }}" class="form-control"
+                                    placeholder=" تحلیل وضعیت درآمد سرانه را وارد کنید...">
                             </div>
                         </div>
 
-                        <x-select-year :default="old('year')" :required="false" name="year"></x-select-year>
+                        <x-select-year :default="old('year')" min="1390" max="1400" :required="false" name="year"></x-select-year>
 
-                        <x-select-month :default="old('month')" :required="false" name="month"></x-select-month>
+                        {{-- <x-select-month :default="old('month')" :required="false" name="month"></x-select-month> --}}
 
 
 
