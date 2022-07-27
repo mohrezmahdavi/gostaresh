911514bafc7d90c9e14871538416e2b9475affa2
mahdavi <m.mahdavi@raahin.com>
2022-07-27 16:39:52 +0430

    GOS-251 fix

--- resources/views/admin/gostaresh/graduates-of-higher-education/create/create.blade.php
+++ resources/views/admin/gostaresh/graduates-of-higher-education/create/create.blade.php
@@ -1,20 +1,23 @@
-{{--Table 32 View--}}
+{{-- Table 32 View --}}
 @extends('layouts.dashboard')
 
 @section('title-tag')
-ایجاد کل دانش آموختگان از مراکز آموزش عالی موجود در شھرستان محل استقرار واحد دانشگاھی (فارغ التحصیلان کل زیرنظام ھا) به تفکیک مقطع و گروه عمده تحصیلی و جنسیت
+    ایجاد کل دانش آموختگان از مراکز آموزش عالی موجود در شھرستان محل استقرار واحد دانشگاھی (فارغ التحصیلان کل زیرنظام ھا) به
+    تفکیک مقطع و گروه عمده تحصیلی و جنسیت
 @endsection
 
 @section('breadcrumb-title')
-ایجاد کل دانش آموختگان از مراکز آموزش عالی موجود در شھرستان محل استقرار واحد دانشگاھی (فارغ التحصیلان کل زیرنظام ھا) به تفکیک مقطع و گروه عمده تحصیلی و جنسیت
+    ایجاد کل دانش آموختگان از مراکز آموزش عالی موجود در شھرستان محل استقرار واحد دانشگاھی (فارغ التحصیلان کل زیرنظام ھا) به
+    تفکیک مقطع و گروه عمده تحصیلی و جنسیت
 @endsection
 
 @section('page-title')
-ایجاد کل دانش آموختگان از مراکز آموزش عالی موجود در شھرستان محل استقرار واحد دانشگاھی (فارغ التحصیلان کل زیرنظام ھا) به تفکیک مقطع و گروه عمده تحصیلی و جنسیت
+    ایجاد کل دانش آموختگان از مراکز آموزش عالی موجود در شھرستان محل استقرار واحد دانشگاھی (فارغ التحصیلان کل زیرنظام ھا) به
+    تفکیک مقطع و گروه عمده تحصیلی و جنسیت
 
-<span>
-    <a href="{{ route('admin.index') }}" class="btn btn-info btn-sm">بازگشت به منو</a>
-</span>
+    <span>
+        <a href="{{ route('admin.index') }}" class="btn btn-info btn-sm">بازگشت به منو</a>
+    </span>
 @endsection
 
 @section('styles-head')
@@ -26,15 +29,21 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body" id="app">
-                    <form class="form-horizontal" method="POST" action="{{ route('graduates-of-higher-education.store') }}" role="form">
+                    <form class="form-horizontal" method="POST" action="{{ route('graduates-of-higher-education.store') }}"
+                        role="form">
                         @csrf
 
-                        <select-province-component
-                            province_default="{{ auth()->user()->province_id ?? '' }}"
-                            zone_default="{{ auth()->user()->county->zone ?? ''}}"
+                        <select-province-component province_default="{{ auth()->user()->province_id ?? '' }}"
+                            zone_default="{{ auth()->user()->county->zone ?? '' }}"
                             county_default="{{ auth()->user()->county_id ?? '' }}"
                             city_default="{{ auth()->user()->city_id ?? '' }}"
-                            rural_district_default="{{ auth()->user()->rural_district_id ?? '' }}">
+                            rural_district_default="{{ auth()->user()->rural_district_id ?? '' }}"
+                            :fields="{{ json_encode([
+                                'province' => true,
+                                'zone' => false,
+                                'county' => true,
+                                'city' => false,
+                            ]) }}">
                         </select-province-component>
 
                         <select-grade-component></select-grade-component>
@@ -45,21 +54,21 @@
                                 <span class="text-danger" style="font-size: 11px !important"> (اجباری) </span>
                             </label>
                             <div class="col-sm-10">
-                                <input type="text" id="university" name="university"
-                                       value="{{ old('university') }}" class="form-control"
-                                       placeholder=" دانشگاه را وارد کنید...">
+                                <input type="text" id="university" name="university" value="{{ old('university') }}"
+                                    class="form-control" placeholder=" دانشگاه را وارد کنید...">
                             </div>
                         </div>
 
                         <div class="form-group row mt-2">
                             <label class="col-sm-2 col-form-label" for="gender_id">
-                                <span>جنسیت  </span>&nbsp
+                                <span>جنسیت </span>&nbsp
                                 <span class="text-danger" style="font-size: 11px !important"> (اجباری) </span>
                             </label>
                             <div class="col-sm-10">
                                 <select name="gender_id" id="gender_id" class="form-select">
                                     @foreach (config('gostaresh.gender') as $key => $value)
-                                        <option {{ ($key == old('gender_id') ? 'selected' : '') }} value="{{ $key }}">{{ $value }}</option>
+                                        <option {{ $key == old('gender_id') ? 'selected' : '' }}
+                                            value="{{ $key }}">{{ $value }}</option>
                                     @endforeach
                                 </select>
 
@@ -68,13 +77,14 @@
 
                         <div class="form-group row mt-2">
                             <label class="col-sm-2 col-form-label" for="department_of_education">
-                                <span>گروه عمده تحصیلی  </span>&nbsp
+                                <span>گروه عمده تحصیلی </span>&nbsp
                                 <span class="text-danger" style="font-size: 11px !important"> (اجباری) </span>
                             </label>
                             <div class="col-sm-10">
-                                <select name="department_of_education" id="department_of_education" class="form-select" >
+                                <select name="department_of_education" id="department_of_education" class="form-select">
                                     @foreach (config('gostaresh.department_of_education') as $key => $value)
-                                        <option {{ ($key == old('department_of_education') ? 'selected' : '') }} value="{{ $key }}">{{ $value }}</option>
+                                        <option {{ $key == old('department_of_education') ? 'selected' : '' }}
+                                            value="{{ $key }}">{{ $value }}</option>
                                     @endforeach
                                 </select>
 
@@ -88,8 +98,8 @@
                             </label>
                             <div class="col-sm-10">
                                 <input type="number" id="associate_degree" name="associate_degree"
-                                       value="{{ old('associate_degree') }}" class="form-control"
-                                       placeholder=" کاردانی را وارد کنید...">
+                                    value="{{ old('associate_degree') }}" class="form-control"
+                                    placeholder=" کاردانی را وارد کنید...">
                             </div>
                         </div>
 
@@ -100,8 +110,8 @@
                             </label>
                             <div class="col-sm-10">
                                 <input type="number" id="bachelor_degree" name="bachelor_degree"
-                                       value="{{ old('bachelor_degree') }}" class="form-control"
-                                       placeholder=" کارشناسی را وارد کنید...">
+                                    value="{{ old('bachelor_degree') }}" class="form-control"
+                                    placeholder=" کارشناسی را وارد کنید...">
                             </div>
                         </div>
 
@@ -111,9 +121,8 @@
                                 <span class="text-danger" style="font-size: 11px !important"> (اجباری) </span>
                             </label>
                             <div class="col-sm-10">
-                                <input type="number" id="masters" name="masters"
-                                       value="{{ old('masters') }}" class="form-control"
-                                       placeholder=" کارشناسی ارشد را وارد کنید...">
+                                <input type="number" id="masters" name="masters" value="{{ old('masters') }}"
+                                    class="form-control" placeholder=" کارشناسی ارشد را وارد کنید...">
                             </div>
                         </div>
 
@@ -123,15 +132,14 @@
                                 <span class="text-danger" style="font-size: 11px !important"> (اجباری) </span>
                             </label>
                             <div class="col-sm-10">
-                                <input type="number" id="phd" name="phd"
-                                       value="{{ old('phd') }}" class="form-control"
-                                       placeholder=" دکتری را وارد کنید...">
+                                <input type="number" id="phd" name="phd" value="{{ old('phd') }}"
+                                    class="form-control" placeholder=" دکتری را وارد کنید...">
                             </div>
                         </div>
 
-                        <x-select-year :default="old('year')" :required="false" name="year"></x-select-year>
+                        <x-select-year :default="old('year')" min="1390" max="1400" :required="false" name="year"></x-select-year>
 
-                        <x-select-month :default="old('month')" :required="false" name="month"></x-select-month>
+                        {{-- <x-select-month :default="old('month')" :required="false" name="month"></x-select-month> --}}
 
                         <button type="submit" class="btn btn-primary  mt-3">افزودن</button>
                     </form>
