09ca70e4f05f649b58e8d3a3799801accc2dba4a
hadikhah <ma.hadikhah@raahin.com>
2022-08-23 21:01:15 +0430

    add international-student-growth-rate line chart

---
+++ app/View/Components/Gostaresh/InternationalStudentGrowthRate/LineChartAllFieldsByYearComponent.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\View\Components\Gostaresh\InternationalStudentGrowthRate;
+
+use App\Models\Index\InternationalStudentGrowthRate;
+use Comma\Lachart\Classes\Lachart;
+use Exception;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class LineChartAllFieldsByYearComponent extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return View
+     * @throws Exception
+     */
+    public function render()
+    {
+        $modelQuery = InternationalStudentGrowthRate::whereRequestsQuery();
+
+        $chart_arr2 = [
+            'chart_title'         => "کاردانی",
+            'report_type'         => 'group_by_string',
+            'model'               => $modelQuery,
+            'group_by_field'      => 'year',
+            'chart_type'          => 'line',
+            'aggregate_function'  => 'sum',
+            'aggregate_field'     => 'kardani_count',
+            'aggregate_transform' => function ($value) {
+                return $value;
+            },
+        ];
+        $chart_arr3 = [
+            'chart_title'         => "کارشناسی",
+            'report_type'         => 'group_by_string',
+            'model'               => $modelQuery,
+            'group_by_field'      => 'year',
+            'chart_type'          => 'line',
+            'aggregate_function'  => 'sum',
+            'aggregate_field'     => 'karshenasi_count',
+            'aggregate_transform' => function ($value) {
+                return $value;
+            },
+        ];
+        $chart_arr4 = [
+            'chart_title'         => "کارشناسی ارشد",
+            'report_type'         => 'group_by_string',
+            'model'               => $modelQuery,
+            'group_by_field'      => 'year',
+            'chart_type'          => 'line',
+            'aggregate_function'  => 'sum',
+            'aggregate_field'     => 'karshenasi_arshad_count',
+            'aggregate_transform' => function ($value) {
+                return $value;
+            },
+        ];
+        $chart_arr5 = [
+            'chart_title'         => "دکتری",
+            'report_type'         => 'group_by_string',
+            'model'               => $modelQuery,
+            'group_by_field'      => 'year',
+            'chart_type'          => 'line',
+            'aggregate_function'  => 'sum',
+            'aggregate_field'     => 'docktora_count',
+            'aggregate_transform' => function ($value) {
+                return $value;
+            },
+        ];
+        $chart = new Lachart($chart_arr2, $chart_arr3, $chart_arr4, $chart_arr5);
+
+        return view('components.gostaresh.international-student-growth-rate.line-chart-all-fields-by-year-component', compact("chart"));
+    }
+}
