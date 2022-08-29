1f68c81b31b0351daa0df5595fb06ef18644130d
hadikhah <ma.hadikhah@raahin.com>
2022-08-29 11:42:45 +0430

    add line charts title

--- resources/views/components/gostaresh/average-test-score-of-the-last-five-percent-of-admitted/line-chart-all-fields-by-year-component.blade.php
+++ resources/views/components/gostaresh/average-test-score-of-the-last-five-percent-of-admitted/line-chart-all-fields-by-year-component.blade.php
@@ -1,6 +1,17 @@
 @if (isset($chart))
     <div class="card">
         <div class="card-body">
+            <div class="d-flex align-items-start justify-content-between">
+                <div>
+                    <h4 class="header-title">نمودار خطی مجموع مقدار میانگین رتبه آزمون 5 درصد آخر پذیرفته شدگان بر حسب
+                        سال</h4>
+                </div>
+                <div class="avatar-sm">
+                    <span class="avatar-title bg-soft-primary rounded">
+                        <i class="ri-line-chart-fill font-20 text-primary"></i>
+                    </span>
+                </div>
+            </div>
             {!! $chart->renderHtml() !!}
 
             {!! $chart->renderChartJsLibrary() !!}
